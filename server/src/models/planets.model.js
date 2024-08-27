4137669626f6ce85165378542fcc6142988e1aec
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-08-27 13:30:15 +0600

    added planets data to show in front end side

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,42 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, "..", "data", "kepler_data.csv"))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (!isHabitablePlanet(data)) return;
+        habitablePlanets.push(data);
+      })
+      .on("error", (error) => {
+        reject(error);
+      })
+      .on("end", () => {
+        console.log(habitablePlanets.map((planet) => planet["kepler_name"]));
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,22 @@
 const http = require("http");
 const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+async function startServer() {
+  // Loading planets data on start server
+  try {
+    await loadPlanetsData();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+
+startServer();
