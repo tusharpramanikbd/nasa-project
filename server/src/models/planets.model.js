c81526f8ed5c3e09eb69de609180b19d680f8ad6
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-09-04 11:20:34 +0600

    saved and find planets data from mongodb

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
 
-const habitablePlanets = [];
+const planets = require("./planets.mongo");
 
 const isHabitablePlanet = (planet) => {
   return (
@@ -22,22 +22,41 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (!isHabitablePlanet(data)) return;
-        habitablePlanets.push(data);
+        await savePlanet(data);
       })
       .on("error", (error) => {
         reject(error);
       })
-      .on("end", () => {
-        console.log(habitablePlanets.map((planet) => planet["kepler_name"]));
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found`);
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (error) {
+    console.error("Could not save planet", error);
+  }
 }
 
 module.exports = {
