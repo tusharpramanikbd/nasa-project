40a3fd60cda49a98a620358b285cf1b950f61465
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-09-13 12:29:20 +0600

    added pagination to get all launches

---
+++ server/src/services/query.js
@@ -0,0 +1,17 @@
+const DEFAULT_PAGE_NUMBER = 1;
+const DEFAULT_PAGE_LIMIT = 0; // in mongo, it will return all data if limit is 0
+
+function getPagination(query) {
+  const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
+  const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
+  const skip = (page - 1) * limit;
+
+  return {
+    skip,
+    limit,
+  };
+}
+
+module.exports = {
+  getPagination,
+};
